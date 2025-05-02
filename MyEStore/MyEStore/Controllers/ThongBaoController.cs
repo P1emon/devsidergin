4d00db9d4661c4f3bbd99470abb5c1a4512b9481
P1emon <khang113324@gmail.com>
2025-05-02 22:09:20 +0700

    Add thong bao controller

---
+++ MyEStore/MyEStore/Controllers/ThongBaoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+public class ThongBaoController : Controller
+{
+    private ISliderThongBaoService _service;
+    public ThongBaoController(ISliderThongBaoService service)
+    {
+        _service = service;
+    }
+
+    // Giao diện hiển thị slider sắp diễn ra
+    public IActionResult Index()
+    {
+        var sliders = _service.GetSlidersSapDienRa();
+        return View(sliders);
+    }
+
+    // Route test gửi email ngay lập tức
+    [HttpGet]
+    public IActionResult TestGuiEmail()
+    {
+        _service.GuiEmailThongBao();
+        return Content("✅ Đã test gửi email cho toàn bộ khách hàng thành công.");
+    }
+
+
+}
